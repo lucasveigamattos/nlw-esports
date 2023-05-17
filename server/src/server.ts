7e56d197f19064633a730c583f5344a06ed084ee
Lucas Veiga Mattos <lucasveigamattos@gmail.com>
2023-05-17 18:06:40 -0300

    server setup

---
+++ server/src/server.ts
@@ -0,0 +1,11 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT
+
+app.listen(port, () => {
+    console.log(`> Server running on port: ${port}`)
+})
